6518bb66fa76b66c58bb590414a548ea6750653f
NachoSupreme <majedchaaban@gmail.com>
2015-09-24 14:50:03 -0400

    added blackjack

---
+++ BlackJack.java
@@ -0,0 +1,16 @@
+class Cards {
+	String name;
+	String value;
+	String cards;
+	
+
+	public static void main(String[] args) {
+	Cards[] myCards = new Cards[5];
+		myCards[0] = new Cards();
+		myCards[1] = new Cards();
+		myCards[2] = new Cards();
+		myCards[3] = new Cards();
+		myCards[4] = new Cards();
+
+}
+}
